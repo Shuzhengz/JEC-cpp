4edceaf91e935d42cdec152653303dd1348227db
Shuzhengz <treez.zhang@gmail.com>
2022-08-05 17:35:43 -0700

    adds rm and mk to dir and files

--- library.hpp
+++ library.hpp
@@ -1,10 +1,34 @@
 #ifndef JEC_CPP_LIBRARY_HPP
 #define JEC_CPP_LIBRARY_HPP
 
-class Config {};
+#include <string>
 
-class ConfigFile {};
+namespace library {
 
-class ConfigDir {};
+    class Config {
+    protected:
+
+    public:
+        static bool exists (std::string& path);
+
+    };
+
+    class ConfigFile : public Config{
+    public:
+        void remove(std::string& path);
+
+        void create(std::string& path, const char name);
+
+        static void from_home();
+    };
+
+    class ConfigDir : public Config {
+        void remove(std::string& path);
+
+        void create(std::string& path, char name);
+
+        static void from_home();
+    };
+}
 
 #endif //JEC_CPP_LIBRARY_HPP
